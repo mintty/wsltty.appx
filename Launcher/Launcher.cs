609d955ddbcbfc0507cdc0c05931bbe699f7ed2c
Biswapriyo Nath <nathbappai@gmail.com>
2018-03-15 01:36:24 +0530

    Initialize wsltty.appx with required files (#1)
    * add launcher file
    
    set OutputType in Visual Studio project to "Windows Application" so that a console window does not popup i.e. WinExe in .csproj file
    
    * add NewTux.svg file
    
    This SVG file is required to create Assets for Appx package. Source: https://commons.wikimedia.org/wiki/File:NewTux.svg and Based on original image by Larry Ewing, made in GIMP. Attribution: lewing@isc.tamu.edu Larry Ewing and The GIMP
    
    * add Assets and icon
    
    * add XML files

---
+++ Launcher/Launcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Windows.Storage;
+
+namespace Launcher
+{
+    class Launcher
+    {
+        static void Main()
+        {
+            string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            int index = location.LastIndexOf("\\");
+            string exePath = location.Substring(0, index);
+
+            string wslbridge = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\wslbridge";
+            Directory.CreateDirectory(wslbridge);
+            string[] files = new string[] { "cygwin1.dll", "wslbridge.exe", "wslbridge-backend" };
+            foreach (string file in files)
+            {
+                File.Copy($"{exePath}\\{file}", $"{wslbridge}\\{file}", true);
+            }
+
+            string mintty = $"{exePath}\\mintty.exe";
+            string wslbridgePath = $"{ApplicationData.Current.LocalCacheFolder.Path}\\Roaming\\wslbridge\\wslbridge.exe";
+            string configPath = $"{ApplicationData.Current.LocalCacheFolder.Path}\\Roaming\\mintty.config";
+            Process.Start(
+                mintty,
+                $" --config {configPath} --icon \"{exePath}\\wsltty.ico\" --exec {wslbridgePath}  -C ~"
+                );
+        }
+    }
+}
+
+/*END-35*/
